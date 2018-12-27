623fe0eb6dd4b613434c9690a04b6f1435b49791
ZiCHENG Wang <wangzc5514@163.com>
2018-12-28 01:17:56 +0800

    Update analysis.py

--- 下半学期作业/analysis.py
+++ 下半学期作业/analysis.py
@@ -31,10 +31,10 @@ for err2 in errorlist2:
 
 errorset1 = set(infos1).intersection(set(infos2))
 errorset2 = set(infos1).union(set(infos2))
-print(len(errorset1))
-print(len(errorset2))
+print('set1 length : ',len(errorset1))
+print('set2 length : ',len(errorset2))
 # print(errorset1)
-print('sames ',len(errorset1))
+print('sames links length :',len(errorset1))
 diff = []
 for err1 in errorlist1:
     title = err1[0]
@@ -45,47 +45,47 @@ for err1 in errorlist1:
                 if err1[i] != err2[i]:
                     diff.append(err1[0]+err1[1]+','+err1[i]+','+err2[i]+'\n')
 
-print(diff)
-print(len(diff))
+#print(diff)
+print('different link length : ',len(diff))
 
 # with open('similarity.csv','w',encoding='utf8') as ff:
 
 
 
-# browser = 'chrome'
-# filename1 = 'errorlist-chrome.csv'
-# browser = 'firefox'
-# filename2 = 'errorlist-firefox.csv'
-# errorlist = []
-#
-# with open(filename2, encoding='utf8') as f:
-#     reader = csv.reader(f)
-#     errorlist = (list(reader))
-# for item in errorlist:
-#     if item[2]!= '':
-#         count[0] = count[0]+1
-#     if item[3] != '':
-#         count[1] = count[1]+1
-#     if item[4] != '':
-#         count[2] = count[2]+1
-#     if item[5] != '':
-#         count[3] = count[3]+1
-#
-# print(count)
-#
-# import numpy as np
+browser = 'chrome'
+filename1 = 'errorlist-chrome.csv'
+browser = 'firefox'
+filename2 = 'errorlist-firefox.csv'
+errorlist = []
+
+with open(filename2, encoding='utf8') as f:
+    reader = csv.reader(f)
+    errorlist = (list(reader))
+for item in errorlist:
+    if item[2]!= '':
+        count[0] = count[0]+1
+    if item[3] != '':
+        count[1] = count[1]+1
+    if item[4] != '':
+        count[2] = count[2]+1
+    if item[5] != '':
+        count[3] = count[3]+1
+
+print(count)
+
+import numpy as np
 import matplotlib.pyplot as plt
-# labels = [['http','https'],['diff','no diff'],['not belong','belong'],['not loaded','loaded']]
-# fracs = [[count[0], sum-count[0]], [count[1], sum-count[1]],
-#          [count[2], sum-count[2]], [count[3], sum-count[3]]]
-# plt.axes(aspect=1)
-#
-#
-# for i in range(4):
-#     plt.pie(x=fracs[i], labels=labels[i], shadow=True, autopct='%3.2f %%')
-#
-#     plt.savefig(browser+str(i)+'.png')
-#     plt.show()
+labels = [['http','https'],['diff','no diff'],['not belong','belong'],['not loaded','loaded']]
+fracs = [[count[0], sum-count[0]], [count[1], sum-count[1]],
+         [count[2], sum-count[2]], [count[3], sum-count[3]]]
+plt.axes(aspect=1)
+
+
+for i in range(4):
+    plt.pie(x=fracs[i], labels=labels[i], shadow=True, autopct='%3.2f %%')
+
+    plt.savefig(browser+str(i)+'.png')
+    plt.show()
 
 labels = ['same','not same']
 fracs = [635-45,45]
